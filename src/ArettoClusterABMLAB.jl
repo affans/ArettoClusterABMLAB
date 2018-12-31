6bc73f3b25518a26b830b412f498555f18bf0a17
affan <affan.s@outlook.com>
2018-12-30 22:31:04 -0500

    minor update

--- src/ArettoClusterABMLAB.jl
+++ src/ArettoClusterABMLAB.jl
@@ -5,12 +5,17 @@ module ArettoClusterABMLAB
     import Distributed.launch, Distributed.manage, Distributed.kill, Distributed.init_worker, Distributed.connect
     export ArettoManager
     export launch, manage
-    worker_arg = `--worker`
+    
+    #worker_arg = `--worker`
 
-    function __init__()
-        global worker_arg
-        worker_arg = `--worker=$(Distributed.cluster_cookie())`
-    end
+    #function __init__()
+    #    global worker_arg
+    #    worker_arg = `--worker=$(Distributed.cluster_cookie())`
+    #end
+    
+
+    worker_arg() = `--worker=$(Distributed.init_multi(); cluster_cookie())`
+
 
     struct ArettoManager <: ClusterManager
         np::Integer
@@ -18,41 +23,44 @@ module ArettoClusterABMLAB
 
     function launch(manager::ArettoManager, params::Dict, launched::Array, c::Condition)
         try
+            # cleanup old files
+            map(rm, filter(t -> occursin(r"job.*\.out", t), readdir(exehome)))
+
             ## get the default arguments passed to addprocs()  
             ## filter the non-default keys from stdkeys
             ## the filtered keys are the ones that are sent to Slurm
             ## TODO if jobname supplied in params, don't supply default name.
             ## TODO workerarg can be passed in exeflags
             stdkeys  = keys(Distributed.default_addprocs_params()) 
-            p = filter((x,y) -> !(x in stdkeys), params) #get the keys passed in params that are not in stdkeys
+            p = filter(x->!(x[1] in stdkeys), params)
             srunargs = []
             for k in keys(p)
-                ## if branch for either -N or --name options for Slurm
-                if length(string(k)) == 1
-                    push!(srunargs, "-$k")
-                    val = p[k]
-                    if length(val) > 0
-                        push!(srunargs, "$(p[k])")
-                    end
-                else
-                    k2 = replace(string(k), "_" => "-")
-                    val = p[k]
-                    if length(val) > 0
-                        push!(srunargs, "--$(k2)=$(p[k])")
-                    else
-                        push!(srunargs, "--$(k2)")
-                    end
-                end
+                if length(string(k)) == 1    
+                    push!(srunargs, "-$k")    
+                    val = p[k]    
+                    if length(val) > 0    
+                        push!(srunargs, "$(p[k])")    
+                    end    
+                else    
+                    k2 = replace(string(k), "_"=>"-")    
+                    val = p[k]    
+                    if length(val) > 0    
+                        push!(srunargs, "--$(k2)=$(p[k])")    
+                    else    
+                        push!(srunargs, "--$(k2)")    
+                    end    
+                end    
             end
    
             exehome  = params[:dir]
             exename  = params[:exename]
             exeflags = params[:exeflags] 
             
+            
             np = manager.np
             jobname = "julia-$(getpid())" ## use default jobname.. 
       
-            srun_cmd = `srun -J $jobname -n $np -o "job%4t.out" -D $exehome $(srunargs) $exename $exeflags $worker_arg`
+            srun_cmd = `srun -J $jobname -n $np -o "job%4t.out" -D $exehome $(srunargs) $exename $exeflags $(worker_arg())`
             srun_proc = open(srun_cmd)
            
             @debug "passed in params: $params"
@@ -111,6 +119,7 @@ module ArettoClusterABMLAB
         # with :interrupt when interrupt(workers) is called. The ClusterManager should signal the appropriate worker with an interrupt signal.
         # with :finalize for cleanup purposes.
     end
+    addprocs_slurm(np::Integer; kwargs...) = addprocs(ArettoManager(np); kwargs...)
 end
 
 #using .Aretto
